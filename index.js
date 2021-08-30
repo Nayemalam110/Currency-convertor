475902a36a4da921c8fe510992a65339077e74ff
nayemalam110 <nayemalam110@gmail.com>
2021-08-30 14:21:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// var euro1=document.getElementById('euro').value;
+// euro1=parseInt(euro1);
+// console.log(euro1);
+// var usd1=document.getElementById('usd');
+// console.log(usd1);
+// var GBP=document.getElementById('fbp');
+// console.log(GBP);
+// var kes=document.getElementById('kes');
+// console.log(kes);
+
+
